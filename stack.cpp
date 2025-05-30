b47eb4b67a7a1b56ca8aec10b6666236375ce3cf
muftifaiyazshamim <muftifaiyazshamim@gmail.com>
2025-05-30 17:24:29 +0600

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -39,26 +39,57 @@ void isfull(){
    if (top==N-1){
     cout <<"Full"<<endl;
    }
+   else{
+   cout << "not full" << endl;
+   }
 
 }
 void isempty(){
     if (top==-1){
     cout <<"Empty"<<endl;
    }
+   else{
+   cout << "Not Empty" << endl;
+   }
 
 }
 
 int main(){
 
+int f;
+for(;;){
+cout << "Enter the number what you want to do : \n1.Push \n2.Pop \n3.Display \n4.isFull \n5.isEmpty \n6.Exit" << endl;
+cin >> f;
+if(f==1)
+    {
+    push();
+    }
 
-    push();
-    push();
-     push();
-      push();
-       push();
-    display();
-isfull();
+else if(f==2)
+    {
     pop();
-    pop();
+    }
+else if(f==3)
+    {
     display();
+    }
+else if(f==4)
+    {
+    isfull();
+    }   
+else if(f==5)
+    {
+    isempty();
+    }
+else if(f==6)
+    {
+    cout << "Exiting..." << endl;
+    break;
+    }
+else
+    {
+    cout << "Please enter a valid number(between 1 to 6)" << endl;
+    }
+ }    
+return 0;
 }
